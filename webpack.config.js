da6074f31bedac4e4bbaf5add12b79510b5188f4
Andrea Bogazzi <andreabogazzi79@gmail.com>
2018-05-11 01:42:53 +0200

    test to remove external deps

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,13 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
-  }
+  },
+  externals: {
+    'canvas-prebuilt': 'undefined',
+    'canvas': 'undefined',
+    'jsdom/lib/jsdom/utils': JSON.stringify({ Canvas: null }),
+    'jsdom/lib/jsdom/living/generated/utils': JSON.stringify({ implForWrapper: null }),
+    'jsdom': 'null',
+    'xmldom': JSON.stringify({ DOMParser: null }),
+  },
 };
